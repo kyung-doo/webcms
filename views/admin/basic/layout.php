e126073fe22c8e17813a3f976900a11cb810e122
BO Choi <form4526@gmail.com>
2016-07-18 12:35:53 +0900

    스킨설정 메뉴 삭제

--- views/admin/basic/layout.php
+++ views/admin/basic/layout.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>CIBoard Admin</title>
+<title>Administraton</title>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" />
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap-theme.min.css'); ?>" />
 <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
